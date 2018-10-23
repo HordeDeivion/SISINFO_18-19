2a5cc81de210948e5b8b6baecf19d8c21662a25a
Nacpac <nachoooxd98@gmail.com>
2018-10-23 13:26:27 +0200

    Add files via upload

---
+++ borrar_tablas.sql
@@ -0,0 +1,12 @@
+--AUTORES
+--	Ignacio Palacios Gracia		NIP:739359
+--	David Ubide Alaiz		NIP:736520
+--	José Manuel Romero		NIP:740914
+
+drop table reto;
+drop table respuesta;
+drop table pregunta;
+drop table comentario;
+drop table cartel;
+drop table alumno;
+drop table profesor;
